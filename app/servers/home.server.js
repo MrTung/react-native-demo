03643feda1d566436409d39f5a59fdfde75aade2
dongxuwei <287178790@qq.com>
2020-10-15 11:49:44 +0800

    icons导致报错问题修复

--- app/servers/home.server.js
+++ app/servers/home.server.js
@@ -1,6 +1,8 @@
 import { request } from '../utils/request';
 
-const { host } = process.env.APPCONFIG;
+// const { host } = process.env.APPCONFIG;
+
+const host = 'https://tech-dev.api.fuliaoyi.com';
 
 /**
  * 测试请求
